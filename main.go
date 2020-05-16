470e1e4960e302068aafadf6fc00bc6a6695406d
Bekket <jackatwork@yandex.ru>
2020-05-16 14:18:11 +0300

    added session and test calls for session

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
-//	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 
 //	"os"
     "context"
@@ -35,6 +36,8 @@ func main(){
 //	gw := myenv["GATEWAY"]
 //	fmt.Println(gw)
 
+    pk := myenv["PK"] // load private key from env
+
   //  client, err := ethclient.Dial(os.Getenv("GATEWAY"))	// for global env config
 	client, err := ethclient.Dial(myenv["GATEWAY"])			// load from local .env file
     if err != nil {
@@ -42,15 +45,20 @@ func main(){
     }
     defer client.Close()
 
+
+    // check calls 
+    // check balance
     accountAddress := common.HexToAddress("0x892eE0398C9d8C86BCA3ffa49c33b68A7b2F38d3")
     balance, _ := client.BalanceAt(ctx, accountAddress, nil)
 	fmt.Printf("Balance: %d\n",balance)
-	
+    
+    // Setting up Deposit Contract
 	deposit, err := dep.NewDeposit(common.HexToAddress("0xdD5A73bfE8f907D6592197cF4Ba5945b1bFc608f"), client)
 	if err != nil {
 		log.Fatalf("Failed to instantiate a Deposit contract: %v", err)
 	}
 
+    // Check view calls (owner address)
 	owner,err := deposit.Owner(nil)
 	if err != nil {
 		log.Fatalf("Failed to owner of contract: %v", err)
@@ -58,6 +66,43 @@ func main(){
 
 	owner_human := owner.Hex()
 
-	fmt.Println(owner_human)
+    fmt.Println(owner_human)
+    
+    // setting up private key in proper format
+    privateKey, err := crypto.HexToECDSA(pk)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Creating an auth transactor
+    auth := bind.NewKeyedTransactor(privateKey)
+    
+    // Wrap the Deposit contract instance into a session
+      session := &dep.DepositSession{
+	Contract: deposit,
+	CallOpts: bind.CallOpts{
+        Pending: true,
+        From: auth.From,
+        Context: context.Background(),
+	},
+	TransactOpts: bind.TransactOpts{
+		From:     auth.From,
+		Signer:   auth.Signer,
+        GasLimit: 0,
+        GasPrice: nil,
+	},
+}
+
+// check call for getting owner inside session
+    ownr,err := session.Owner()
+    if err != nil {
+    log.Fatalf("Failed to owner of contract: %v", err)
+    }
+    oh:= ownr.Hex()
+    fmt.Println(oh)
+
+
+
+
 
 }
