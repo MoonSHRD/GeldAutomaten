1861eb9ce55c69832fd1174555601b7a20400d8a
JackBekket <jackatwork@yandex.ru>
2020-05-21 01:32:16 +0300

    Chore/draft (#8)
    * draft
    
    * refactored few controller functions
    
    * chore events
    
    * cleanup

--- example/main.go
+++ example/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/ethereum/go-ethereum/event"
 	"math/big"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/common"
@@ -31,66 +30,17 @@ func loadEnv() {
 }
 
 
-// create cashInRequest (fiat -> blockchain)
-func CashInRequest(_session *dep.DepositSession, wallet common.Address, _uuid string, _amount *big.Int) {
-
-    
-    txCashInRequest, err := _session.CashInRequest(wallet,_uuid,_amount)
-    if err != nil {
-        log.Printf("could not send cash IN submit to contract: %v\n", err)
-    }
-    fmt.Printf("CashIN Submit sent! Please wait for tx %s to be confirmed.\n", txCashInRequest.Hash().Hex())
-
-}
-
-
-
-// submit, that we recive funds from user and maked cash_out of this funds to user card. 
-func CashOutSubmit(_session *dep.DepositSession, txid *big.Int) {
-
-    txSubmitRequest,err := _session.CashOutSubmit(txid)
-    if err != nil {
-        log.Printf("could not send cash out submit to contract: %v\n", err)
-        
-    }
-    fmt.Printf("CashOut Submit sent! Please wait for tx %s to be confirmed.\n", txSubmitRequest.Hash().Hex())
-}
-
-// revert - we have accepted cashOutSubmit from user, but can't proceed it transaction to his payment card
-func CashOutRevert(_session *dep.DepositSession, txid *big.Int, errmsg string) {
-
-    txRevertRequest,err := _session.CashOutRevert(txid, errmsg)
-    if err != nil {
-        log.Printf("could not send cash out submit to contract: %v\n", err)
-        
-    }
-    fmt.Printf("CashOut Submit sent! Please wait for tx %s to be confirmed.\n", txRevertRequest.Hash().Hex())
-}
-
-
-func SubcribeCashOutAnonymouse(_session *dep.DepositSession, _ch chan *dep.DepositCashOutRequestEventAnonymouse) (event.Subscription, error) {
-
-    cash_out_filter := _session.Contract.DepositFilterer
-    subscription,err := cash_out_filter.WatchCashOutRequestEventAnonymouse(&bind.WatchOpts{
-            Start: nil, //last block
-            Context: nil,
-    },_ch)
-    if err != nil {
-        log.Printf("error due subscription to event")
-            log.Fatalln(err)
-    }
-    return subscription, err
-
-
-}
 
 func main(){
     loadEnv()
 
     ctx := context.Background()
+
+//	gw := myenv["GATEWAY"]
+//	fmt.Println(gw)
+
     pk := myenv["PK"] // load private key from env
 
-    // Connecting to network
   //  client, err := ethclient.Dial(os.Getenv("GATEWAY"))	// for global env config
 	client, err := ethclient.Dial(myenv["GATEWAY"])			// load from local .env file
     if err != nil {
@@ -99,27 +49,43 @@ func main(){
     defer client.Close()
 
 
- // setting up private key in proper format
-    privateKey, err := crypto.HexToECDSA(pk)
-        if err != nil {
-            log.Fatal(err)
-        }
-    
-// Creating an auth transactor
-    auth := bind.NewKeyedTransactor(privateKey)
-
     // check calls 
     // check balance
     accountAddress := common.HexToAddress("0x892eE0398C9d8C86BCA3ffa49c33b68A7b2F38d3")
-    balance, _ := client.BalanceAt(ctx, accountAddress, nil)    //our balance
+    balance, _ := client.BalanceAt(ctx, accountAddress, nil)
 	fmt.Printf("Balance: %d\n",balance)
     
-
     // Setting up Deposit Contract
 	deposit, err := dep.NewDeposit(common.HexToAddress("0xf460e21B21d294f6832aF166862c38E6Be71f423"), client)
 	if err != nil {
 		log.Fatalf("Failed to instantiate a Deposit contract: %v", err)
     }
+    
+
+
+  
+
+
+
+
+    // Check view calls (owner address)
+	owner,err := deposit.Owner(nil)
+	if err != nil {
+		log.Fatalf("Failed to owner of contract: %v", err)
+	}
+
+	owner_human := owner.Hex()
+
+    fmt.Println(owner_human)
+    
+    // setting up private key in proper format
+    privateKey, err := crypto.HexToECDSA(pk)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Creating an auth transactor
+    auth := bind.NewKeyedTransactor(privateKey)
     
     // Wrap the Deposit contract instance into a session
       session := &dep.DepositSession{
@@ -132,8 +98,8 @@ func main(){
 	TransactOpts: bind.TransactOpts{
 		From:     auth.From,
 		Signer:   auth.Signer,
-        GasLimit: 0,                    // 0 automatically estimates gas limit
-        GasPrice: nil,                  // nil automatically suggests gas price
+        GasLimit: uint64(5000000),
+        GasPrice: big.NewInt(1),
         Context: context.Background(),
     },
 }
@@ -143,25 +109,38 @@ func main(){
     Events
     */
 
-    // Check retriving events of CashOut request
+    // Check retriving (past) events of CashOut request
 
     var ch = make(chan *dep.DepositCashOutRequestEventAnonymouse)
-    subscription, err := SubcribeCashOutAnonymouse(session,ch)
+    cash_out_filter := session.Contract.DepositFilterer
+   // cash_out_filter.WatchCashOutRequestEventAnonymouse(nil,ch)
+    subscription,err := cash_out_filter.WatchCashOutRequestEventAnonymouse(&bind.WatchOpts{
+            Start: nil,
+            Context: nil,
+    },ch)
+    if err != nil {
+        log.Printf("error due subscription to event")
+            log.Fatalln(err)
+    }
+
+    
 
 
 // check call for getting owner inside session
-/*
     ownr,err := session.Owner()
     if err != nil {
     log.Fatalf("Failed to owner of contract: %v", err)
     }
     oh:= ownr.Hex()
     fmt.Println(oh)
-*/
+
 
     // check Cash out request
     _purce := "address karty"
     _payment_method := "karta"
+    /*
+    txOutRequest,err := session.CashOutRequest(_purce,_payment_method),big.NewInt(1)
+    */
 
     txOutRequest,err := deposit.CashOutRequest(&bind.TransactOpts{
         From: auth.From,
@@ -183,11 +162,15 @@ func main(){
 
 
     // check Cash Out Submit
+   // _tx_out_id := 2
    /*
-    _tx_out_id := 2
-    CashOutSubmit(session,_tx_out_id) 
+    txSubmitRequest,err := session.CashOutSubmit(big.NewInt(2))
+    if err != nil {
+        log.Printf("could not send cash out submit to contract: %v\n", err)
+        
+    }
+    fmt.Printf("CashOut Submit sent! Please wait for tx %s to be confirmed.\n", txSubmitRequest.Hash().Hex())
     */
-    
 
 
     // check cash in request
@@ -195,17 +178,86 @@ func main(){
     user_wallet := common.HexToAddress("0x892eE0398C9d8C86BCA3ffa49c33b68A7b2F38d3")
     uuid_in := "1"
     amount_in := big.NewInt(1)
-    CashInRequest(session,user_wallet,uuid_in,amount_in)
+
+
+    txCashInRequest, err := session.CashInRequest(user_wallet,uuid_in,amount_in)
+    if err != nil {
+        log.Printf("could not send cash IN submit to contract: %v\n", err)
+    }
+    fmt.Printf("CashOut Submit sent! Please wait for tx %s to be confirmed.\n", txCashInRequest.Hash().Hex())
+
 
  
-   
-    // Print result of events, we got during subscription
+    //cash_out_filter := session.Contract.WatchCashOutRequestEventAnonymouse(nil,)
+    
+   // fmt.Printf("subscription:")
+   // fmt.Printf(subscription)
+
     event_result := <-ch
+
     fmt.Println("/n")
     fmt.Println("Destination for cash_out:", event_result.Purce)
     fmt.Println("Amount for cash_out:", event_result.Amount)
     subscription.Unsubscribe()
+
+    /*
+    for {
+        select {
+        case err := <-subscription.Err():
+            log.Printf("error due subscription to event")
+            log.Fatalln(err)
+        
+          
+    case event_log := <-ch :
+        fmt.Println("Destination: ")
+
+        }
+
+    }
+    */
+
+    /*
+    for {
+		select {
+		case err := <-subscription.Err():
+			log.Fatal(err)
+		case log := <-ch:
+			var greetEvent struct {
+				Name  string
+				Count *big.Int
+			}
+
+			err = greeterAbi.Unpack(&greetEvent, "_Greet", log.Data)
+
+			if err != nil {
+				fmt.Println("Failed to unpack:", err)
+			}
+
+			fmt.Println("Contract:", log.Address.Hex())
+			fmt.Println("Name:", greetEvent.Name)
+			fmt.Println("Count:", greetEvent.Count)
+		}
+	}
+    */
+
+
+
 }
 
+/*
+// cash out request (for test purposes)
+func OutRequest(session &dep.DepositSession, string _purce, string _paymentMethod) {
+    
+    txOutRequest, err := session.
+}
+*/
 
+
+/*
+// cash out request (for test purposes)
+func OutRequest(&dep.DepositSession session, string _purce, string _paymentMethod) {
+    
+    txOutRequest, err := session.
+}
+*/
 
