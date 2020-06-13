492ad23d48ed6194806a65b1c5e251b5b5956d40
ChronosX88 <chronosx88@gmail.com>
2020-06-13 21:39:43 +0400

    Fix some docs and one method (now it returns error as well)

--- geld_automaten.go
+++ geld_automaten.go
@@ -1,8 +1,6 @@
 package ga
 
 import (
-	"fmt"
-	"log"
 	"math/big"
 
 	dep "github.com/MoonSHRD/GeldAutomaten/artifacts/Deposit"
@@ -12,7 +10,7 @@ import (
 )
 
 // CashInRequest is used to create cash-in request (fiat -> blockchain)
-// Returns ID of cash-in transaction
+// Returns ID of cash-in transaction in hex representation
 func CashInRequest(session *dep.DepositSession, wallet common.Address, uuid string, amount *big.Int) (string, error) {
 	txCashInRequest, err := session.CashInRequest(wallet, uuid, amount)
 	if err != nil {
@@ -21,18 +19,18 @@ func CashInRequest(session *dep.DepositSession, wallet common.Address, uuid stri
 	return txCashInRequest.Hash().Hex(), nil
 }
 
-// CashOutSubmit is used to confirm, that we recive funds from user and maked cash_out of this funds to user card.
-func CashOutSubmit(_session *dep.DepositSession, txid *big.Int) {
-	txSubmitRequest, err := _session.CashOutSubmit(txid)
+// CashOutSubmit is used to confirm, that we recive funds from user and did cash out of this funds to user card
+// Returns ID of cash-out transaction in hex representation
+func CashOutSubmit(session *dep.DepositSession, txid *big.Int) (string, error) {
+	txSubmitRequest, err := session.CashOutSubmit(txid)
 	if err != nil {
-		log.Printf("could not send cash out submit to contract: %v\n", err)
-
+		return "", err
 	}
-	fmt.Printf("CashOut Submit sent! Please wait for tx %s to be confirmed.\n", txSubmitRequest.Hash().Hex())
+	return txSubmitRequest.Hash().Hex(), nil
 }
 
 // CashOutRevert is used in the situation when we have accepted cash-out request from user, but can't proceed it transaction to his payment card
-// Returns ID of revert transaction in hex representation
+// Returns ID of revert cash-out transaction in hex representation
 func CashOutRevert(_session *dep.DepositSession, txid *big.Int, errmsg string) (string, error) {
 	txRevertRequest, err := _session.CashOutRevert(txid, errmsg)
 	if err != nil {
